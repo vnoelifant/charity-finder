87e20817303c3c35bdfff690040b4bd3233711d8
vnoelifant <veronicanoel12490@gmail.com>
2022-11-14 22:02:08 -0600

    added featured projects api call

--- charity_finder/views.py
+++ charity_finder/views.py
@@ -2,7 +2,7 @@ import xmltodict
 from django.shortcuts import render
 from django.http import HttpResponse
 from pprint import pprint
-
+from functools import partial
 
 from charity_finder import charity_api
 
@@ -10,10 +10,15 @@ from charity_finder import charity_api
 def home(request):
 
     themes = charity_api.get_charity_data("/themes")
-    # pprint("TRENDING: ", trending)
 
     themes = xmltodict.parse(themes.content)
     print(themes)
-    context = {"themes": themes}
-    
+
+    feature_projects = charity_api.get_charity_data("/featured/projects")
+
+    feature_projects = xmltodict.parse(feature_projects.content)
+    print(feature_projects)
+
+    context = {"themes": themes, "featured_projects": feature_projects}
+
     return render(request, "home.html", context)
