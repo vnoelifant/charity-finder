7f0de213d49960333de35db9f45183452c011fff
vnoelifant <veronicanoel12490@gmail.com>
2022-11-25 15:54:12 -0600

    updated gitignore

--- charity_finder/charity_api.py
+++ charity_finder/charity_api.py
@@ -2,13 +2,15 @@ import os
 import json
 from pprint import pprint
 from functools import partial
-# from utils import dump_charity_data_to_json
 
 import requests
 from dotenv import load_dotenv
 from django.conf import settings
 
 load_dotenv()
+
+CHARITY_API_KEY = os.getenv("PROJECT_API_KEY")
+
 
 BASE_URL = "https://api.globalgiving.org/api/public/projectservice"
 
@@ -20,7 +22,7 @@ def get_charity_data(endpoint):
 
     print("URL: ", url)
 
-    params = {"api_key": settings.CHARITY_API_KEY}
+    params = {"api_key": CHARITY_API_KEY}
 
     # https://www.globalgiving.org/api/how-to-use/tutorial/
     headers = {'Accept': 'application/json'}
@@ -29,9 +31,14 @@ def get_charity_data(endpoint):
     response = requests.get(url, params=params, headers=headers)
     return response.json()
 
+def main():
+    from utils import dump_charity_data_to_json
+     # Returns all GlobalGiving themes, under which projects are categorized
+    data = get_charity_data("/themes")
+    dump_charity_data_to_json("themes.json", data)
+
+
 
 if __name__ == "__main__":
-    # Returns all GlobalGiving themes, under which projects are categorized
-    data = get_charity_data("/themes")
-    print(data)
-    #dump_charity_data_to_json("output.json", data)
+    main()
+   
