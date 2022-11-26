8067459940b2f926dbf8218be133aafa8d206da0
vnoelifant <veronicanoel12490@gmail.com>
2022-11-25 18:44:50 -0600

    added comments

--- charity_finder/models.py
+++ charity_finder/models.py
@@ -9,13 +9,15 @@ class Theme(models.Model):
         """String for representing the Model object."""
         return f"{self.name}: {self.theme_id}"
 
-class Country(models.Model)
+
+class Country(models.Model):
     name = models.CharField(max_length=200)
     country_code = models.CharField(max_length=200)
 
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.name}: {self.country_code}"
+
 
 class Org(models.Model):
     name = models.CharField(max_length=200)
@@ -27,16 +29,18 @@ class Org(models.Model):
     logoUrl = models.CharField(max_length=200)
     addressLine1 = models.CharField(max_length=200)
     addressLine2 = models.CharField(max_length=200)
+    # City where organization resides.
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
     postal = models.CharField(max_length=200)
+    # Country where organization resides.
+    country_home = models.ForeignKey(Country)
+    # one or more themes for this organization
     theme = models.ForeignKey(Theme)
     url = models.CharField(max_length=200)
+    # one or more countries the organization operates in
     country_op = models.ForeignKey(Country)
-    country_home = models.ForeignKey(Country)
 
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.name}: {self.org_id}"
-    
-
