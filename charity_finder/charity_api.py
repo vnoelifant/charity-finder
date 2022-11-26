1e103cab0d66fcd1e21c9ce90ffd51ce7b75c2d6
vnoelifant <veronicanoel12490@gmail.com>
2022-11-26 02:29:02 -0600

    removed old code

--- charity_finder/charity_api.py
+++ charity_finder/charity_api.py
@@ -4,11 +4,9 @@ import requests
 from pprint import pprint
 from functools import partial
 from decouple import config
+from django.conf import settings
 
 BASE_URL = "https://api.globalgiving.org/api/public"
-
-CHARITY_API_KEY = config("PROJECT_API_KEY")
-
 
 def get_charity_data(endpoint):
 
@@ -17,22 +15,12 @@ def get_charity_data(endpoint):
 
     print("URL: ", url)
 
-    params = {"api_key": CHARITY_API_KEY}
+    params = {"api_key": settings.CHARITY_API_KEY}
 
     headers = {'Accept': 'application/json'}
 
     response = requests.get(url, params=params, headers=headers)
     return response.json()
 
-def main():
-    from utils import dump_charity_data_to_json
-    # Returns all GlobalGiving themes, under which projects are categorized
-    # theme_data = get_charity_data("/projectservice/themes")
-    # dump_charity_data_to_json("themes.json", theme_data)
-    
-    # org_data = get_charity_data("/orgservice/all/organizations/active")
-    # dump_charity_data_to_json("orgs.json", org_data)
 
-if __name__ == "__main__":
-    main()
    
