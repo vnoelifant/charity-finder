19da523cb6ab55dca68e026655f56486d129df03
vnoelifant <veronicanoel12490@gmail.com>
2022-12-01 23:46:50 -0600

    removed unneeded function

--- charity_finder/management/commands/import_organizations.py
+++ charity_finder/management/commands/import_organizations.py
@@ -4,17 +4,6 @@ from django.core.management.base import BaseCommand
 from charity_finder.models import Theme, Organization
 from charity_finder import charity_api
 
-
-def insert_themes():
-    themes = charity_api.get_charity_data("/projectservice/themes")
-    Theme.objects.bulk_create(
-        [
-            Theme(name=theme["name"], theme_id=theme["id"])
-            for theme in themes["themes"]["theme"]
-        ]
-    )
-
-
 def insert_active_orgs():
     with open("output_active_orgs.json") as data_file:
         orgs = json.load(data_file)
