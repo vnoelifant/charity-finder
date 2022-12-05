000e205eeb86c9b1531880500cc420696e08aa94
vnoelifant <veronicanoel12490@gmail.com>
2022-12-04 18:16:16 -0600

    added code to dump bulk active project data

--- scripts/dump_data.py
+++ scripts/dump_data.py
@@ -8,7 +8,7 @@ def dump_charity_data_to_json(output_file, data):
     with open(output_file, "w") as charity_data:
         json.dump(data, charity_data, indent=4, sort_keys=True)
 
-def get_json_data(xml_data):
+def get_json_data_from_xml(xml_data):
     with open(xml_data) as xml_file:
         org_json_data = xmltodict.parse(xml_file.read())
     return org_json_data
@@ -21,7 +21,7 @@ def get_url_from_json(input_file):
     response = requests.get(url) 
     return response
 
-def download_url_data(output_file, response):
+def download_bulk_data_to_xml(output_file, response):
     with open(output_file, 'wb') as file:
         file.write(response.content)
 
@@ -37,17 +37,34 @@ def run():
     org_data = charity_api.get_charity_url_data("/orgservice/all/organizations/active/download")
 
     # Convert the XML URL file to JSON
-    org_json_url = xmltodict.parse(org_data.content)
-    dump_charity_data_to_json("output_orgs_url.json", org_json_url)
+    org_file = xmltodict.parse(org_data.content)
+    dump_charity_data_to_json("output_orgs_url.json", org_file)
 
-    # Get the bulk organization url response from JSON
-    url_response = get_url_from_json('output_orgs_url.json')
+    # Get the bulk organization url from JSON
+    org_url = get_url_from_json('output_orgs_url.json')
     
     # Download bulk organization data to XML file
-    download_url_data("output_active_orgs.xml", url_response)
+    download_bulk_data_to_xml("output_active_orgs.xml", org_url)
     
     # Dump bulk organization data to JSON file
-    dump_charity_data_to_json("output_active_orgs.json", get_json_data("output_active_orgs.xml"))
+    dump_charity_data_to_json("output_active_orgs.json", get_json_data_from_xml("output_active_orgs.xml"))
+
+    # Get an XML file containting a URL of all active projects (bulk data download)
+    project_data = charity_api.get_charity_url_data("/projectservice/all/projects/active/download.xml")
+
+    # Convert the XML URL file to JSON
+    project_file = xmltodict.parse(project_data.content)
+    dump_charity_data_to_json("output_projects_url.json", project_file)
+
+    # Get the bulk project url from JSON
+    project_url= get_url_from_json('output_projects_url.json')
+    
+    # Download bulk project data to XML file
+    download_bulk_data_to_xml("output_active_projects.xml", project_url)
+    
+    # Dump bulk project data to JSON file
+    dump_charity_data_to_json("output_active_projects.json", get_json_data_from_xml("output_active_projects.xml"))
+
    
     
 
