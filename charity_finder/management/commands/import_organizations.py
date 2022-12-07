74d5a33837d85335b80de1ef082dd1ed64ae1f66
vnoelifant <veronicanoel12490@gmail.com>
2022-12-06 22:39:35 -0600

    applied fixes from PR #22

--- charity_finder/management/commands/import_organizations.py
+++ charity_finder/management/commands/import_organizations.py
@@ -5,30 +5,15 @@ from charity_finder.models import Theme, Organization, Country
 from charity_finder import charity_api
 
 
-"""
-def get_matching_data(data_from_json):
-    matching_data = []
-
-    for row in data_from_json:
-        if data_from_json == "themes_from_json":
-            theme, inserted = Theme.objects.get_or_create(name=row["name"], theme_id=row["id"])
-            matching_data.append(theme)
-        else:
-            country, inserted = Country.objects.get_or_create(name=data_from_json["name"], country_code=data_from_json["iso3166CountryCode"])
-            matching_data.append(country)
-
-    return matching_data
-"""
-
-
 def insert_active_orgs():
     with open("output_active_orgs.json") as data_file:
         orgs = json.load(data_file)
         # pprint(orgs['organizations']['organization'])
         # print(len(orgs["organizations"]["organization"])) # 3157
         for org_row in orgs["organizations"]["organization"]:
-            org = Organization.objects.create(
-                name=org_row.get("name", ""),
+            name = org_row.get("name", "")
+            org, created = Organization.objects.get_or_create(
+                name=name,
                 org_id=org_row.get("id", 0),
                 mission=org_row.get("mission", ""),
                 active_projects=org_row.get("activeProjects", 0),
@@ -43,52 +28,51 @@ def insert_active_orgs():
                 country_home=org_row.get("country", ""),
                 url=org_row.get("url", ""),
             )
-
-            themes = org_row.get("themes", dict)
-
-            if themes is None:
+            if not created:
+                print(f"org {name} was already created.")
                 continue
 
-            themes_from_json = themes.get("theme", [])
+            themes = org_row.get("themes")
+            if themes is not None:
+                matching_themes = get_matching_themes(themes)
+                org.themes.add(*matching_themes)
 
-            # matching_themes = get_matching_data(themes_from_json)
-            matching_themes = []
+            countries = org_row.get("countries")
+            if countries is not None:
+                matching_countries = get_matching_countries(countries)
+                org.countries.add(*matching_countries)
 
-            if isinstance(themes_from_json, dict):
-                themes_from_json = [themes_from_json]
 
-            for row in themes_from_json:
-                theme, inserted = Theme.objects.get_or_create(
-                    name=row.get("name", ""), theme_id=row.get("id", "")
-                )
+def get_matching_themes(themes):
+    countries_from_json = themes.get("country", [])
+    matching_themes = []
 
-            matching_themes.append(theme)
-            org.themes.add(*matching_themes)
+    if isinstance(countries_from_json, dict):
+        themes_from_json = [themes_from_json]
 
-            countries = org_row.get("countries", dict)
+    for row in countries_from_json:
+        theme, inserted = Theme.objects.get_or_create(
+            name=row.get("name", ""),
+            theme_id=row.get("id", ""),
+        )
+        matching_themes.append(theme)
+    return matching_themes
 
-            if countries is None:
-                continue
 
-            countries_from_json = countries.get("country", [])
-            # matching_countries = get_matching_data(countries_from_json)
+def get_matching_countries(countries):
+    countries_from_json = countries.get("country", [])
+    matching_countries = []
 
-            matching_countries = []
+    if isinstance(countries_from_json, dict):
+        countries_from_json = [countries_from_json]
 
-            if isinstance(countries_from_json, dict):
-                countries_from_json = [countries_from_json]
-
-            for row in countries_from_json:
-
-                country, inserted = Country.objects.get_or_create(
-                    name=row.get("name", ""),
-                    country_code=row.get("iso3166CountryCode", ""),
-                )
-
-            matching_countries.append(country)
-
-            org.countries.add(*matching_countries)
-
+    for row in countries_from_json:
+        country, inserted = Country.objects.get_or_create(
+            name=row.get("name", ""),
+            country_code=row.get("iso3166CountryCode", ""),
+        )
+        matching_countries.append(country)
+    return matching_countries
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -103,4 +87,4 @@ class Command(BaseCommand):
         if options["model"] == "org":
             print("Seeding organization data")
             insert_active_orgs()
-        print("completed")
+        print("Completed")
--- charity_finder/models.py
+++ charity_finder/models.py
@@ -23,6 +23,7 @@ class Country(models.Model):
 
 class Organization(models.Model):
     name = models.CharField(max_length=200,null=True, blank=True)
+    type = models.CharField(max_length=200,null=True, blank=True)
     org_id = models.IntegerField(default=0,null=True, blank=True)
     mission = models.TextField(default="", null=True, blank=True)
     active_projects = models.IntegerField(default=0,null=True, blank=True)
@@ -43,12 +44,48 @@ class Organization(models.Model):
         """String for representing the Model object."""
         return f"{self.name}: {self.org_id}"
 
+"""
+
 class Project(models.Model):
     org = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200,null=True, blank=True)
-    # ...
+    summary = models.TextField(default="", null=True, blank=True)
+    project_id = models.IntegerField(default=0,null=True, blank=True)
+    project_link = models.CharField(max_length=200,null=True, blank=True)
+    active = models.BooleanField(null=True, blank=True)
+    status = models.CharField(max_length=200,null=True, blank=True)
+    activities = models.TextField(default="", null=True, blank=True)
+    approved_date = models.DateTimeField(null=True, blank=True)
+    contact_address = models.CharField(max_length=200,null=True, blank=True)
+    contact_city = models.CharField(max_length=200,null=True, blank=True)
+    contact_country = models.CharField(max_length=200,null=True, blank=True)
+    contact_name = models.CharField(max_length=200,null=True, blank=True)
+    contact_postal = models.CharField(max_length=200,null=True, blank=True)
+    contact_state = models.CharField(max_length=200,null=True, blank=True)
+    contact_url = models.CharField(max_length=200,null=True, blank=True)
+    countries = models.ManyToManyField(Country, related_name='countries',blank=True)
+    country_home = models.CharField(max_length=200,null=True, blank=True)
+    date_report = models.DateTimeField(null=True, blank=True)
+    donation_options = models.JSONField(default=dict, null = True, blank=True)
+    funding = models.DecimalField(max_digits = 5,decimal_places = 2)
+    goal = models.DecimalField(max_digits = 5,decimal_places = 2)
+    remaining = models.DecimalField(max_digits = 5,decimal_places = 2)
+    image = models.JSONField(default=dict, null = True, blank=True)
+    long_term_impact = models.BooleanField(null=True, blank=True)
+    need = models.BooleanField(null=True, blank=True)
+    modified_date = models.DateTimeField(null=True, blank=True)
+    number_donations = models.IntegerField(default=0,null=True, blank=True)
+    number_reports = models.IntegerField(default=0,null=True, blank=True)
+    progress_report_link = models.CharField(max_length=200,null=True, blank=True)
+    themes = models.ManyToManyField(Theme, related_name='themes', blank=True)
+    theme_name = models.CharField(max_length=200,null=True, blank=True)
+    videos = models.JSONField(default=dict, null = True, blank=True)
+    latitude = models.DecimalField(max_digits = 5,decimal_places = 2)
+    longitude = models.DecimalField(max_digits = 5,decimal_places = 2)
+    notice = models.TextField(default="", null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+
+"""
+
 
     
