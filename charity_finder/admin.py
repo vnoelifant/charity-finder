c7cafea7cbc2c21b3bb5a93d27468cf4bc9ac3d6
vnoelifant <veronicanoel12490@gmail.com>
2022-12-08 00:42:44 -0600

    models and admin update to include projects and regions

--- charity_finder/admin.py
+++ charity_finder/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from charity_finder.models import Theme, Country, Organization
+from charity_finder.models import Theme, Country, Organization, Region, Project
 
 
 # Register your models here.
@@ -7,16 +7,43 @@ from charity_finder.models import Theme, Country, Organization
 # admin.site.register(Country)
 # admin.site.register(Organization)
 
+
 @admin.register(Theme)
 class ThemeAdmin(admin.ModelAdmin):
     list_display = ("name", "theme_id")
 
+
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ("name","country_code")
+    list_display = (
+        "name",
+        "country_code",
+    )
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("name","org_id")
-    search_fields = ('name',)
-    list_filter = ("themes", "countries")
+    list_display = (
+        "name",
+        "org_id",
+    )
+    search_fields = ("name",)
+    list_filter = (
+        "themes",
+        "countries",
+    )
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "project_id",
+    )
+    search_fields = ("title",)
+    list_filter = ("region",)
--- charity_finder/models.py
+++ charity_finder/models.py
@@ -10,82 +10,98 @@ class Theme(models.Model):
         """String for representing the Model object."""
         return f"{self.name}: {self.theme_id}"
 
+
 class Country(models.Model):
     name = models.CharField(max_length=200)
     country_code = models.CharField(max_length=200)
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.name}: {self.country_code}"
+
     class Meta:
         verbose_name_plural = "countries"
 
 
+class Region(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "regions"
+
+
 class Organization(models.Model):
-    name = models.CharField(max_length=200,null=True, blank=True)
-    type = models.CharField(max_length=200,null=True, blank=True)
-    org_id = models.IntegerField(default=0,null=True, blank=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    type = models.CharField(max_length=200, null=True, blank=True)
+    org_id = models.IntegerField(default=0, null=True, blank=True)
     mission = models.TextField(default="", null=True, blank=True)
-    active_projects = models.IntegerField(default=0,null=True, blank=True)
-    total_projects = models.IntegerField(default=0,null=True, blank=True)
-    ein = models.CharField(max_length=200,null=True, blank=True)
-    logo_url = models.CharField(max_length=200,null=True, blank=True)
-    address_line1 = models.CharField(max_length=200,null=True, blank=True)
+    active_projects = models.IntegerField(default=0, null=True, blank=True)
+    total_projects = models.IntegerField(default=0, null=True, blank=True)
+    ein = models.CharField(max_length=200, null=True, blank=True)
+    logo_url = models.CharField(max_length=200, null=True, blank=True)
+    address_line1 = models.CharField(max_length=200, null=True, blank=True)
     address_line2 = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200,null=True, blank=True)
-    state = models.CharField(max_length=200,null=True, blank=True)
-    postal = models.CharField(max_length=200,null=True, blank=True)
-    country_home = models.CharField(max_length=200,null=True, blank=True)
-    themes = models.ManyToManyField(Theme, related_name='themes', blank=True)
-    url = models.CharField(max_length=200,null=True, blank=True)
-    countries = models.ManyToManyField(Country, related_name='countries',blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    state = models.CharField(max_length=200, null=True, blank=True)
+    postal = models.CharField(max_length=200, null=True, blank=True)
+    country_home = models.CharField(max_length=200, null=True, blank=True)
+    themes = models.ManyToManyField(Theme, related_name="themes", blank=True)
+    url = models.CharField(max_length=200, null=True, blank=True)
+    countries = models.ManyToManyField(Country, related_name="countries", blank=True)
 
     def __str__(self):
         """String for representing the Model object."""
         return f"{self.name}: {self.org_id}"
 
-"""
 
 class Project(models.Model):
-    org = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200,null=True, blank=True)
+    org = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, null=True, blank=True
+    )
+    title = models.CharField(max_length=200, null=True, blank=True)
     summary = models.TextField(default="", null=True, blank=True)
-    project_id = models.IntegerField(default=0,null=True, blank=True)
-    project_link = models.CharField(max_length=200,null=True, blank=True)
+    project_id = models.IntegerField(default=0, null=True, blank=True)
+    project_link = models.CharField(max_length=200, null=True, blank=True)
     active = models.BooleanField(null=True, blank=True)
-    status = models.CharField(max_length=200,null=True, blank=True)
+    status = models.CharField(max_length=200, null=True, blank=True)
     activities = models.TextField(default="", null=True, blank=True)
     approved_date = models.DateTimeField(null=True, blank=True)
-    contact_address = models.CharField(max_length=200,null=True, blank=True)
-    contact_city = models.CharField(max_length=200,null=True, blank=True)
-    contact_country = models.CharField(max_length=200,null=True, blank=True)
-    contact_name = models.CharField(max_length=200,null=True, blank=True)
-    contact_postal = models.CharField(max_length=200,null=True, blank=True)
-    contact_state = models.CharField(max_length=200,null=True, blank=True)
-    contact_url = models.CharField(max_length=200,null=True, blank=True)
-    countries = models.ManyToManyField(Country, related_name='countries',blank=True)
-    country_home = models.CharField(max_length=200,null=True, blank=True)
+    contact_address_1 = models.CharField(max_length=200, null=True, blank=True)
+    contact_address_2 = models.CharField(max_length=200, null=True, blank=True)
+    contact_city = models.CharField(max_length=200, null=True, blank=True)
+    contact_country = models.CharField(max_length=200, null=True, blank=True)
+    contact_name = models.CharField(max_length=200, null=True, blank=True)
+    contact_title = models.CharField(max_length=200, null=True, blank=True)
+    contact_postal = models.CharField(max_length=200, null=True, blank=True)
+    contact_state = models.CharField(max_length=200, null=True, blank=True)
+    contact_url = models.CharField(max_length=200, null=True, blank=True)
+    countries = models.ManyToManyField(Country, related_name="countries", blank=True)
+    country_home = models.CharField(max_length=200, null=True, blank=True)
+    country_home_code = models.CharField(max_length=200, null=True, blank=True)
     date_report = models.DateTimeField(null=True, blank=True)
-    donation_options = models.JSONField(default=dict, null = True, blank=True)
-    funding = models.DecimalField(max_digits = 5,decimal_places = 2)
-    goal = models.DecimalField(max_digits = 5,decimal_places = 2)
-    remaining = models.DecimalField(max_digits = 5,decimal_places = 2)
-    image = models.JSONField(default=dict, null = True, blank=True)
+    donation_options = models.JSONField(default=dict, null=True, blank=True)
+    funding = models.DecimalField(max_digits=5, decimal_places=2)
+    goal = models.DecimalField(max_digits=5, decimal_places=2)
+    goal_remaining = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.JSONField(default=dict, null=True, blank=True)
     long_term_impact = models.BooleanField(null=True, blank=True)
     need = models.BooleanField(null=True, blank=True)
     modified_date = models.DateTimeField(null=True, blank=True)
-    number_donations = models.IntegerField(default=0,null=True, blank=True)
-    number_reports = models.IntegerField(default=0,null=True, blank=True)
-    progress_report_link = models.CharField(max_length=200,null=True, blank=True)
-    themes = models.ManyToManyField(Theme, related_name='themes', blank=True)
-    theme_name = models.CharField(max_length=200,null=True, blank=True)
-    videos = models.JSONField(default=dict, null = True, blank=True)
-    latitude = models.DecimalField(max_digits = 5,decimal_places = 2)
-    longitude = models.DecimalField(max_digits = 5,decimal_places = 2)
+    number_donations = models.IntegerField(default=0, null=True, blank=True)
+    number_reports = models.IntegerField(default=0, null=True, blank=True)
+    progress_report_link = models.CharField(max_length=200, null=True, blank=True)
+    themes = models.ManyToManyField(Theme, related_name="themes", blank=True)
+    theme_name = models.CharField(max_length=200, null=True, blank=True)
+    videos = models.JSONField(default=dict, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
     notice = models.TextField(default="", null=True, blank=True)
+    region = models.ManyToManyField(Theme, related_name="regions", blank=True)
 
-
-"""
-
-
-    
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.title}: {self.project_id}"
