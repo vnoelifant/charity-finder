73f0e55f789bae5174c11fb19e412de9d5cdf2fa
vnoelifant <veronicanoel12490@gmail.com>
2023-01-01 16:16:23 -0600

    added changes from PR #31 related to heatmap

--- charity_finder/management/commands/import_organizations.py
+++ charity_finder/management/commands/import_organizations.py
@@ -132,8 +132,7 @@ def insert_active_projects():
                 project.videos = video_url
 
             # parse images dictionary for image link
-            images = project_row.get("image").get("imagelink", [])
-
+            images = project_row.get("image", {}).get("imagelink", [])
             if images is not None:
                 if isinstance(images, dict):
                     images = [images]
@@ -146,12 +145,14 @@ def insert_active_projects():
 
             # get matching organization from foreign key relationship to Organization model
             organization = project_row.get("organization")
-            
+
             # try query outside the loop and give back a dictionary
             if organization is not None:
                 org_id = organization.get("id", "")
                 try:
-                    org = Organization.objects.get(org_id=org_id) # doing this for every loop row
+                    org = Organization.objects.get(
+                        org_id=org_id
+                    )  # doing this for every loop row
                 except Organization.DoesNotExist:
                     print("SKIP: cannot find org id so skipping project", org)
                     continue
--- charity_finder/urls.py
+++ charity_finder/urls.py
@@ -7,8 +7,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("project_detail/<int:org_id>/", views.get_project_detail, name="project_detail"),
+    path(
+        "project_detail/<int:org_id>/", views.get_project_detail, name="project_detail"
+    ),
     path("orgs_theme/", views.get_orgs_by_theme, name="orgs_theme"),
     path("orgs_search/", views.search, name="orgs_search"),
     path("heat_map/", views.heat_map, name="heat_map"),
+    path("heat_map_data", views.heat_map_data, name="heat_map_data"),
 ]
