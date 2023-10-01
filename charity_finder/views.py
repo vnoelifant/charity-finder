ad5d7311b31f045fbea9c444c1695b4a9eaf687f
vnoelifant <veronicanoel12490@gmail.com>
2023-10-01 18:40:33 -0500

    refactor

--- charity_finder/views.py
+++ charity_finder/views.py
@@ -2,85 +2,99 @@ import folium
 from folium.plugins import HeatMap
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
-from django.db.models import Avg, Max, Min, Sum
-from pprint import pprint
-from functools import partial
-from typing import Final
+from django.db.models import Max
+from typing import List, Tuple, Final
 
 from charity_finder.models import Theme, Organization, Project
 from charity_finder import charity_api
 
-# Create your views here.
-def home(request):
-
-    project_map = get_map()
-
-    context = {
-        "project_map": project_map,
-    }
-    return render(request, "home.html", context)
-
-
-def get_map():
-
-    # Filtering by remaining funding money by Region
-    # TODO:  Include more regions
-    GOAL_LIMIT: Final[
-        int
-    ] = 100_000  # constant marked final, can't assign another value to it
-    REGION_NAME: Final[str] = "Africa"
-    # Starting Latitude, Longitude coordinates
-    LAT_LON_INIT: Final[list] = [59.09827437369457, 13.115860356662202]
-
-    projects = Project.objects.filter(
-        goal_remaining__gte=GOAL_LIMIT, region__name=REGION_NAME
-    )
-
-    # For normalizing data for heat map
-    goal_remaining_max = projects.aggregate(Max("goal_remaining"))
-
-    project_map = folium.Map(location=LAT_LON_INIT, zoom_start=3)
-
+def add_heat_points_and_popups(project_map: folium.Map, projects: List[Project], goal_remaining_max) -> None:
+    """
+    Adds heat points and popups to the given Folium map based on the provided list of projects.
+    
+    Args:
+        project_map (folium.Map): The Folium map object to which heat points and popups will be added.
+        projects (List[Project]): A list of project objects containing data for the heat points and popups.
+    """
     for project in projects:
         if project.has_map_data:
+            # Normalizing the goal_remaining value for the heat map
+            goal_norm = float(project.goal_remaining / goal_remaining_max)
 
-            # Normalize data for heat map
-            goal_norm = float(
-                project.goal_remaining / goal_remaining_max["goal_remaining__max"]
-            )
+            # Creating a list of latitude, longitude, and normalized goal_remaining for the heat point data
+            heat_map_data = [[int(project.latitude), int(project.longitude), goal_norm]]
 
-            heat_map_data = [
-                [
-                    int(project.latitude),
-                    int(project.longitude),
-                    goal_norm,
-                ],
-            ]
+            # Adding a heat point data to the map
+            HeatMap(heat_map_data).add_to(project_map)
 
-            title = project.title
-            url = project.project_link
-            goal_remaining = int(project.goal_remaining)
-
+            # Creating HTML content for the popup with project details
             html = f"""
-                    <b>Project Title:</b>{title}<br>
-                    <a href={url} target="_blank">Project Link</a><br>
-                    <b>Funding Needed:</b>{goal_remaining}
-                    <b>Funding Weight:</b>{goal_norm}
-                   """
+                    <b>Project Title:</b>{project.title}<br>
+                    <a href={project.project_link} target="_blank">Project Link</a><br>
+                    <b>Funding Needed:</b>{project.goal_remaining}
+                    <b>Funding Weight:</b>{int(project.goal_remaining)}
+                    """
 
+            # Creating an iframe with the HTML content and adding it as a popup to the map
             iframe = folium.IFrame(html, width=200, height=100)
-
             popup = folium.Popup(iframe, max_width=200)
-
+            # Add a clickable marker at a specified location on the map, which displays a popup 
             folium.Marker(
                 location=[int(project.latitude), int(project.longitude)],
                 tooltip="Click to view Project Summary",
                 popup=popup,
             ).add_to(project_map)
-            HeatMap(heat_map_data).add_to(project_map)
+            
 
-    project_map = project_map._repr_html_()
+def get_map() -> folium.Map:
+    """
+    Creates and returns a Folium heat map object highlighting underserved regions.
+    The map is filtered by remaining funding money by Region and normalized data for the heat map.
+    The function also adds popups to the map with embedded iframes containing project details.
+    
+    Returns:
+        folium.Map: A Folium map object with heat points and popups based on project data.
+    """
+
+    # Constants
+    GOAL_LIMIT: Final[int] = 100_000  # Minimum goal_remaining to include a project in the map
+    REGION_NAME: Final[str] = "Africa"  # The name of the region to filter projects by
+    LAT_LON_INIT: Final[Tuple[float, float]] = (59.09827437369457, 13.115860356662202)  # Initial coordinates for the map
+    
+    # Fetching and filtering projects from the database
+    projects = Project.objects.filter(goal_remaining__gte=GOAL_LIMIT, region__name=REGION_NAME)
+
+    # Finding the maximum goal_remaining value among the filtered projects for normalization
+    goal_remaining_max = projects.aggregate(Max("goal_remaining"))["goal_remaining__max"]
+    
+    # Create a Folium map object centered at the initial latitude and longitude.
+    project_map = folium.Map(location=LAT_LON_INIT, zoom_start=3)
+
+    # Adding heat points and popups to the map based on the filtered projects
+    add_heat_points_and_popups(project_map, projects, goal_remaining_max)
+    
     return project_map
+
+
+def home(request):
+    """
+    Django view function to render the home page of the application.
+    Calls the get_map function to create a Folium map object and passes it to the home.html template.
+    
+    Args:
+        request (HttpRequest): The HTTP request object containing metadata about the request.
+        
+    Returns:
+        HttpResponse: The HTTP response object used to render the home.html template with the Folium map.
+    """
+    # Creating a Folium map object with the get_map function
+    project_map = get_map()
+    # Creating a context dictionary to pass data to the template
+    
+    context = {"project_map": project_map._repr_html_()}  # Get the HTML representation of the Folium map object
+    
+    # Rendering the home.html template with the context dictionary
+    return render(request, "home.html", context)
 
 
 def discover_orgs(request):
